39f57e9bcd3b8d5f87d8f85fd4ad0877698586ad
Olivier <olivier.maire.jp@gmail.com>
2023-11-30 23:20:54 +0900

    Refactoring App with MudBlazor

--- calibre-net.Client/Program.cs
+++ calibre-net.Client/Program.cs
@@ -1,8 +1,12 @@
 using calibre_net.Client;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddMudServices();
 
 builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
